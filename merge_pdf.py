fe2aa74ff65bbc2f4756256431fac50fb5c6da6d
Armen Juhl <armenjuhl@gmail.com>
2020-06-05 20:33:16 -0700

    initialize repo

---
+++ merge_pdf.py
@@ -0,0 +1,63 @@
+import glob
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from collections import OrderedDict
+from math import floor
+
+
+def merger(output_path, input_paths):
+    pdf_writer = PdfFileWriter()
+    for path in input_paths:
+        pdf_reader = PdfFileReader(path)
+        for page in range(pdf_reader.getNumPages()):
+            pdf_writer.addPage(pdf_reader.getPage(page))
+    with open(output_path, 'wb') as fh:
+        pdf_writer.write(fh)
+
+
+if __name__ == '__main__':
+    paths = glob.glob('/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/ChristmasCarols/PDF/*ChristmasCarols.pdf')
+    new_list = dict()
+    for i in range(1, len(paths)): new_list[str(i)] = ''
+
+    for i, v in enumerate(paths):
+        int_checker = ''
+        for j in v[86:88]:
+            try:
+                int(j)
+                int_checker = int_checker + j
+            except Exception:
+                pass
+
+        new_list[str(int_checker)] = v
+        print('match found')
+    final_list = OrderedDict(new_list.items())
+
+    list_1 = list()
+    list_2 = list()
+    list_3 = list()
+    list_4 = list()
+    list_5 = list()
+    final_dict_len = len(final_list)
+
+    complete_list = {1: list_1, 2: list_2, 3: list_3, 4: list_4, 5: list_5}
+
+    counter_a = 0
+    counter_b = 1
+    for i in final_list.items():
+        if counter_a <= floor(final_dict_len / 5):
+            complete_list[counter_b].append(i[1])
+        else:
+            counter_a = 0
+            counter_b += 1
+            complete_list[counter_b].append(i[1])
+
+        counter_a += 1
+    # def create_ordered_dict(dictionary):
+    #     OrderedDict().append(i)
+
+    merger('christmas_carols_1.pdf', complete_list[1])
+    merger('christmas_carols_2.pdf', complete_list[2])
+    merger('christmas_carols_3.pdf', complete_list[3])
+    merger('christmas_carols_4.pdf', complete_list[4])
+    merger('christmas_carols_5.pdf', complete_list[5])
+
---
+++ scrape.py
@@ -0,0 +1,110 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import os
+import tempfile
+from pdf2image import convert_from_path
+
+
+path = '12A Listen to the Mocking Bird'
+import_directory = '/Users/macbookpro/Downloads/group1Pdfs'
+file_name = path[:-4]
+# new_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{file_name[2:]}Dir"
+
+
+output_filenames = dict()
+
+
+def main():
+    with os.scandir(import_directory) as it:
+        for entry in it:
+            if entry.name[-4:] == '.pdf':
+                print('\n\n\nTRUE')
+                new_file_name = entry.name[3:-4]
+
+                try:
+                    new_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{new_file_name}".replace(' ', '')
+                    # new_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{new_file_name[2:]}".replace(' ', '')
+                    os.mkdir(new_directory)
+                except Exception:
+                    pass
+                try:
+                    pdf_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{new_file_name}/PDF".replace(' ', '')
+                    # pdf_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{file_name[2:]}/PDF".replace(' ', '')
+                    os.mkdir(pdf_directory)
+                except Exception:
+                    pass
+                try:
+                    jpg_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{new_file_name}/JPG".replace(' ', '')
+                    # jpg_directory = f"/Users/macbookpro/Desktop/Clients/KRStringsWebsite/pdfScraper2.py/{file_name[2:]}/JPG".replace(' ', '')
+                    os.mkdir(jpg_directory)
+                except Exception:
+                    pass
+
+                split(entry.path, entry.name, pdf_directory)
+
+                for i in output_filenames.values():
+                    convert_pdf_to_jpg(i, jpg_directory)
+
+
+def split(path, name_of_split, pdf_dir):
+    pdf = PdfFileReader(path)
+    first_pass = True
+
+    for page in range(pdf.getNumPages()):
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(pdf.getPage(page))
+
+        if first_pass:
+            output = f'{pdf_dir}/{name_of_split[3:-4]}_{pdf.getNumPages()}.pdf'.replace(' ', '')
+            output_filenames[page] = output
+        else:
+            output = f'{pdf_dir}/{name_of_split[3:-4]}_{page}.pdf'.replace(' ', '')
+            output_filenames[page] = output
+
+        with open(output, 'wb+') as output_pdf:
+            pdf_writer.write(output_pdf)
+
+        first_pass = False
+
+
+def convert_pdf_to_jpg(filename, jpg_dir):
+
+    with tempfile.TemporaryDirectory() as path:
+        images_from_path = convert_from_path(filename, output_folder=path, last_page=1, first_page=0)
+
+    base_filename = os.path.splitext(os.path.basename(filename))[0] + '.jpg'
+
+    save_dir = f'{jpg_dir}'
+
+    for page in images_from_path:
+        page.save(os.path.join(save_dir, base_filename), 'JPEG')
+
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        # information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+        counter = 0
+        pages = dict()
+
+        while counter <= number_of_pages:
+            pages[str(counter)] = pdf.getPage(counter)
+            counter += 1
+
+        for k, v in pages.items():
+            pages[k] = v.extractText()
+
+    for idx, val in enumerate(pages):
+        write_path = f"./{file_name}page_{idx}.txt"
+        write_file = open(write_path, 'w')
+        write_file.write(val)
+
+    # txt = f"""
+    # {pages}
+    # """
+
+    return
+
+
+if __name__ == '__main__':
+    main()
